2f871c10caee401a30955e0ec9194aa23f4fbbf0
inacioFreitas <inacio245444@gmail.com>
2026-04-03 22:35:00 -0300

    Adiciona componente Hero e integra na HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { BrandLogo } from "../components/BrandLogo";
 import NavItem from "../components/NavItem";
 import NavBarCollapse from "../components/NavBarCollapse";
 import { collectionPoints } from "../data/collectionPoints";
-import { ContactPage } from "./ContactPage";
+import {hero} from "../components/Hero";
 
 export function HomePage() {
   return (
@@ -40,17 +40,7 @@ export function HomePage() {
           </nav>
         </header>
 
-        <div className="d-flex flex-column justify-content-center align-items-center text-center section_1_titulo">
-          <h1 className="display-4">𝑺𝒖𝒔𝒕𝒆𝒏𝒕𝒂𝒃𝒊𝒍𝒊𝒅𝒂𝒅𝒆</h1>
-          <h1 className="display-4">𝒆 𝑺𝒐𝒍𝒊𝒅𝒂𝒓𝒊𝒆𝒅𝒂𝒅𝒆 𝒆𝒎 𝑨𝒄̧𝒂̃𝒐</h1>
-          <h6>Transformando alimentos e vidas</h6>
-          <Link
-            className="btn btn-custom animate__animated animate__pulse animate__infinite text-white text-decoration-none"
-            to="/cadastro-login"
-          >
-            Participe
-          </Link>
-        </div>
+        <hero />
       </section>
 
       <section className="section-2" id="pontos-de-coleta">
